4629c32fcb83d1a64d19ba53e22342bdfe3a006e
Helena Tavares Costa <helenacosta1@MB-Helena-C.local>
2015-05-20 17:47:45 -0300

    Initial Commit

---
+++ buttonImgTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  buttonImgTutorial
+//
+//  Created by Helena Tavares Costa on 5/20/15.
+//  Copyright (c) 2015 HTSJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ buttonImgTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  buttonImgTutorial
+//
+//  Created by Helena Tavares Costa on 5/20/15.
+//  Copyright (c) 2015 HTSJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
