b533d199c14d556aed1a2c536effd52f232d8392
Helena Tavares Costa <helenacosta1@MB-Helena-C.local>
2015-05-22 15:51:17 -0300

    código completo rodando

--- buttonImgTutorial/ViewController.m
+++ buttonImgTutorial/ViewController.m
@@ -13,15 +13,24 @@
 @end
 
 @implementation ViewController
-
+//adicionar isso
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIImage* img1 = [UIImage imageNamed:@"botaoVerprevisao"];
+    [self.button1 setImage:img1 forState:UIControlStateNormal];
+    
 }
 
+//adicionar isso
+/*- (IBAction)touchedButton:(id)sender {
+    UIImage* img1 = [UIImage imageNamed:@"botaoVoltar"];
+    [self.button1 setImage:img1 forState:UIControlStateHighlighted];
+}
+*/
+ 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
 @end
